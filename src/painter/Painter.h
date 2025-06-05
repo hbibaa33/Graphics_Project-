c139b98eec4132bff2fbb8cb16b11b87e974b6f0
ahmedmoukhtar01 <ahmedgamalmoukhtar@icloud.com>
2025-06-05 22:08:27 +0300

    Initial commit

--- src/painter/Painter.h
+++ src/painter/Painter.h
@@ -1,6 +1,7 @@
 //
 // Created by LENOVO on 6/5/2025.
 //
+#include <windows.h>
 
 #ifndef PAINTER_H
 #define PAINTER_H
@@ -8,11 +9,20 @@
 
 
 class Painter {
+protected:
+    COLORREF color = RGB(0, 0, 0);
 public:
-    virtual void onMouseDown();
-    virtual void onMouseUp();
-    virtual void onMouseLeft();
-    virtual void onMouseRight();
+    virtual void onMouseLeftDown(HDC hdc, int x, int y) { }
+    virtual void onMouseLeftUp(HDC hdc, int x, int y) { }
+    virtual void onMouseRightDown(HDC hdc, int x, int y) { }
+    virtual void onMouseRightUp(HDC hdc, int x, int y) { }
+    virtual void handleConsole(HDC hdc) = 0;
+
+    virtual void setColor(COLORREF color) {
+        this->color = color;
+    }
+
+    virtual ~Painter() = default;
 };
 
 
