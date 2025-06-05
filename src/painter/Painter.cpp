d452e27e556b49e6f5a6a16b2af7646c2fd6a9b0
ahmedmoukhtar01 <ahmedgamalmoukhtar@icloud.com>
2025-06-05 20:54:57 +0300

    first commit

---
+++ src/painter/Painter.cpp
@@ -0,0 +1,9 @@
+//
+// Created by LENOVO on 6/5/2025.
+//
+
+#include "Painter.h"
+
+void Painter::onMouseDown() {
+
+}
---
+++ src/painter/Painter.h
@@ -0,0 +1,20 @@
+//
+// Created by LENOVO on 6/5/2025.
+//
+
+#ifndef PAINTER_H
+#define PAINTER_H
+
+
+
+class Painter {
+public:
+    virtual void onMouseDown();
+    virtual void onMouseUp();
+    virtual void onMouseLeft();
+    virtual void onMouseRight();
+};
+
+
+
+#endif //PAINTER_H
